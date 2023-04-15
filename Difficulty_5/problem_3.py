142d28ccd32afb80893d520cc54582fa2ba8076f
Taher Ziyaei <ziaee.taher@gmail.com>
2023-04-16 00:26:42 +0330

    problem 3 from ISC

--- Difficulty_5/problem_3.py
+++ Difficulty_5/problem_3.py
@@ -2,27 +2,24 @@ factors_list = list()
 
 
 def factors(n):
-    for i in range(1, (n + 1) // 2):
-        if n % i == 0:
-            factors_list.append(i)
-    return factors_list
+    for i in range(2, (n + 1) // 2):
+        if n % i == 0 and is_prime(i):
+            yield i
+    #         factors_list.append(i)
+    # return factors_list
 
 
-def prime_factors(lst):
-    prime_list = list()
-    flag = True
-    for item in lst:
-        if flag:
-            prime_list.append(item)
-        flag = True
-        for j in range(2, (item + 1) // 2):
-            if item % j == 0:
-                flag = False
-                break
-    return prime_list
+def is_prime(n):
+    for i in range(2,(n+1)//2):
+        if n%i==0:
+            return False
+    return True
 
 
 # n=600851475143
 n = 13195
-print(f"Factors of {n:,} are :\n{factors(n)}")
-print(f"Prime of factors of {n:,} are :\n{prime_factors(factors(n))}")
+# print(f"Factors of {n:,} are :\n{factors(n)}")
+# print(f"Prime of factors of {n:,} are :\n{prime_factors(factors_list)}")
+
+y=factors(n)
+print(list(y))
