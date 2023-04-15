1273e7e4cbb761e2d82ec58fd9106bb5bdec912f
TAHER_Home <ziaee.taher@gmail.com>
2023-04-15 13:14:19 +0330

    problem 3

--- Difficulty_5/problem_3.py
+++ Difficulty_5/problem_3.py
@@ -1,17 +1,28 @@
-prime_list=list()
+factors_list = list()
+
+
 def factors(n):
-    for i in range(1,(n+1)//2):
-        if n%i==0:
-            prime_list.append(i)
-    return prime_list
+    for i in range(1, (n + 1) // 2):
+        if n % i == 0:
+            factors_list.append(i)
+    return factors_list
+
 
 def prime_factors(lst):
+    prime_list = list()
+    flag = True
     for item in lst:
-        for j in range(2,(item+1)//2):
-            if item%j==0:
+        if flag:
+            prime_list.append(item)
+        flag = True
+        for j in range(2, (item + 1) // 2):
+            if item % j == 0:
+                flag = False
                 break
-            else:
+    return prime_list
+
 
 # n=600851475143
-n=13195
-print(f"Prime factors of {n:,} are :\n{factors(n)}")
+n = 13195
+print(f"Factors of {n:,} are :\n{factors(n)}")
+print(f"Prime of factors of {n:,} are :\n{prime_factors(factors(n))}")
